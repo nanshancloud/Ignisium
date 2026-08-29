2bf94f6982f6b834eae0a9c5295a345a7ef95ec9
EricYeung <nanshancloud@outlook.com>
2026-08-29 10:36:46 +0800

    feat(core): add basic document editing

--- crates/ignisium-core/src/lib.rs
+++ crates/ignisium-core/src/lib.rs
@@ -16,3 +16,27 @@ impl TextRange {
         self.end.0 - self.start.0
     }
 }
+
+pub struct Document {
+    text: String,
+}
+
+impl Document {
+    pub fn new() -> Self {
+        Self {
+            text: String::new(),
+        }
+    }
+
+    pub fn text(&self) -> &str {
+        &self.text
+    }
+
+    pub fn insert(&mut self, offset: TextOffset, text: &str) {
+        self.text.insert_str(offset.0, text);
+    }
+
+    pub fn delete(&mut self, range: TextRange) {
+        self.text.replace_range(range.start.0..range.end.0, "");
+    }
+}
